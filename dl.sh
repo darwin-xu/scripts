a2a759083b59d2875c6a45aaae783e7ae058570a
Darwin <darwin.xu@icloud.com>
2014-12-06 18:00:56 +0800

    Add comments in dl.sh.

--- dl.sh
+++ dl.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
+# Use '/' as the separator, print the last part of the URL, it should be the file name.
 fileName=`echo $1 | awk -F/ '{print $NF}'`
 
+# Default download blocks is 4.
 blocks="4"
-if [ "$#" = 2 ]; then
+
+# If 2 parameters or more.
+if [ "$#" > 1 ]; then
 	blocks=$2
 fi
 
 echo "Try to download $1..."
+
+# Get the file size from the http header "Content-Length", use whitespace and '\r' as the separator.
+# '\r' must be removed unless it will cause error in the following calculation.
 dlSize=`curl -sI $1 | grep Content-Length | awk -F '[ \r]' '{print $2}'`
+
 echo "Total size is : $dlSize"
 blockSize=$((dlSize / blocks))
 
 for (( i = 0; i < blocks; i++ ))
 do
+	# Block begin position.
 	b=$(( i * blockSize ))
+	# Block end position.
 	e=$(( (i + 1) * blockSize - 1 ))
+	# If it is the last block, use the size as the block end position.
 	if [ $i = $((blocks - 1)) ]; then
 		e=$dlSize
 	fi
+
+	# Create a .{pid}_000n.tmp temporary file.
 	cmd="curl -s -r $b-$e $1 -o .$$_$(printf %04d $i).tmp"
 	echo "$i: $cmd"
 	$cmd &
