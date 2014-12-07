ab9183634adb4b458bf4b45940bc229d54f109ef
Darwin <darwin.xu@icloud.com>
2014-12-07 22:07:11 +0800

    Print steps in findup.sh

--- findup.sh
+++ findup.sh
@@ -14,6 +14,7 @@ fi
 scriptPath=`dirname $0`
 
 # Calculate the md5 of files
+echo "1) Calculate the md5 of files..."
 find "$1" -type f -exec $scriptPath/md5file.sh "$1" {} .md5 \;
 
 # Remove the last file
@@ -21,13 +22,17 @@ if [ -f .md5table ]; then
 	rm .md5table
 fi
 
-# Sum the md5 up.
+# Sum the md5 up
+echo "2) Sum the md5 up..."
 find .md5 -type f -exec cat {} >> .md5table \;
 
-# sort the file.
+# Sort the md5 file
+echo "3) Sort the md5 file..."
 sort .md5table > .md5tablesort
 rm .md5table
 
+# Print duplicate files
+echo "4) Print duplicate files..."
 while read line; do
 	# Get the md5 value
 	md5v=`echo $line | awk -F\; '{print $1}'`
@@ -36,10 +41,12 @@ while read line; do
 	# Print out the duplicated file
 	if [ "$lastMd5v"x = "$md5v"x ]; then
 		echo "Find dup file : [$lastFile] <-> [$file]"
-		echo "rm $lastFile" >> rmfilelist
+		echo "rm \"$lastFile\"" >> rmfilelist
 	fi
 
 	# Save for the last
 	lastMd5v=$md5v
 	lastFile=$file
 done < .md5tablesort
+
+echo "5) Please check file : rmfilelist"
