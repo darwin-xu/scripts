d86dd3515e1665e0f59f01dfd2d59b54323ccc01
Darwin <daiwen.xu@ericsson.com>
2014-12-30 03:20:43 +0100

    Reformat, fix warning.

--- las.sh
+++ las.sh
@@ -3,22 +3,18 @@
 # This script used to list all suffixes in the folder
 # $1 The folder to search.
 
-find $1 -type f | awk '
-{
-	n = split($0, a, "\/");
+find $1 -type f | awk ' {
+	n = split($0, a, "/");
 	n1 = match(a[n], /^\./);
-	if (n1 == 0)
-	{
+	if (n1 == 0) {
 		n2 = match(a[n], /\.[^\.]+$/);
-		if (n2 != 0)
-		{
+		if (n2 != 0) {
 			suffix = substr(a[n], n2 + 1, length(a[n]) - n2);
 			m[suffix] += 1;
 		}
 	}
 }
-END
-{
+END {
 	for (k in m)
 		print k, ":", m[k];
 }'
