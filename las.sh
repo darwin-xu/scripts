f618a695edba3d81807f171f6c4f1f643393b291
Darwin <daiwen.xu@ericsson.com>
2015-04-22 08:30:05 +0200

    Change the algorithm of las.sh

--- las.sh
+++ las.sh
@@ -3,18 +3,32 @@
 # This script used to list all suffixes in the folder
 # $1 The folder to search.
 
-find $1 -type f | awk ' {
+find $1 -type f | awk ' 
+BEGIN {
+	maxLen = 0;
+}
+{
+	# Split the file by separator
 	n = split($0, a, "/");
-	n1 = match(a[n], /^\./);
-	if (n1 == 0) {
+	# Match the suffix of file
+	# ".gitignore" will be treat as has no suffix.
+	# ".file.dat" will be treat as has suffix.
+	n1 = match(a[n], /^.+\.[^\.]+$/);
+	if (n1 != 0) {
 		n2 = match(a[n], /\.[^\.]+$/);
-		if (n2 != 0) {
-			suffix = substr(a[n], n2 + 1, length(a[n]) - n2);
-			m[suffix] += 1;
-		}
+		suffix = substr(a[n], n2, length(a[n]) - n2 + 1);
+	}
+	else {
+		suffix = "no suffix";
+	}
+	
+	suffixMap[suffix] += 1;
+	if (maxLen < length(suffix)) {
+		maxLen = length(suffix);
 	}
 }
 END {
-	for (k in m)
-		print k, ":", m[k];
+	format = "%-" maxLen + 1 "s:%3d\n"
+	for (k in suffixMap)
+		printf format, k, suffixMap[k];
 }'
