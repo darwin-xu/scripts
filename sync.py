ec4c5653219b278b4e892a9453c09f172440bf41
Darwin Xu <darwin.xu@icloud.com>
2015-10-02 00:56:52 +0800

    can remove from dest dir

--- sync.py
+++ sync.py
@@ -7,14 +7,28 @@ import shutil
 
 from sys import argv
 
+def force_rmdir(dir) :
+	for root, dirs, files in os.walk(dir, False) :
+		for name in files :
+			os.remove(os.path.join(root, name))
+		for name in dirs :
+			os.rmdir(os.path.join(root, name))
+	os.rmdir(dir)
+
 def walk_dir(srcDir, dstDir, topdown = True) :
 	copyCount = 0
 	skipCount = 0
 	removeCount = 0
-	for root, dirs, srcFiles in os.walk(srcDir, topdown) :
+	for root, srcDirs, srcFiles in os.walk(srcDir, topdown) :
+		# Skip all files start with '.'
 		if not os.path.basename(root).startswith('.') :
+
+			# Get the dest file path and files list.
 			dstPath = os.path.join(dstDir, os.path.relpath(root, srcDir))
+			if not os.path.isdir(dstPath) :
+				os.makedirs(dstPath)
 			dstFiles = os.listdir(dstPath)
+
 			for name in srcFiles :
 				if not name.startswith('.') :
 					try :
@@ -26,20 +40,30 @@ def walk_dir(srcDir, dstDir, topdown = True) :
 					#print dstFileName, os.path.isfile(dstFileName)
 					#statinfo = os.stat(dstFileName)
 					if (not os.path.isfile(dstFileName)) or (os.stat(srcFileName).st_size != os.stat(dstFileName).st_size) :
-						dstFolder = os.path.dirname(dstFileName)
-						if not os.path.isdir(dstFolder) :
-							os.makedirs(dstFolder)
 						print "copy [" + srcFileName + "] -> [" + dstFileName + "]"
 						shutil.copy(srcFileName, dstFileName)
 						copyCount += 1
 					else :
 						#print "skip [" + srcFileName + "]"
 						skipCount += 1
+
+			for name in srcDirs :
+				try :
+					dstFiles.remove(name)
+				except :
+					pass
+
 			for name in dstFiles:
 				fileToRemove = os.path.join(dstPath, name)
-				print "remove [" + fileToRemove + "]"
-				os.remove(fileToRemove)
-				removeCount += 1
+				if os.path.isfile(fileToRemove) :
+					print "remove file [" + fileToRemove + "]"
+					os.remove(fileToRemove)
+					removeCount += 1
+				else :
+					print "remove dir  [" + fileToRemove + "]"
+					force_rmdir(fileToRemove)
+					removeCount += 1
+
 	return copyCount, skipCount, removeCount
 
 beginTime = time.time()
